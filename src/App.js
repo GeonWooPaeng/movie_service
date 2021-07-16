7b85c1d9568ff0041fffef2fc6686b94bf199728
GeonWooPaeng <gwpaeng@naver.com>
2021-07-16 20:45:15 +0900

    react fetch => axios

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { directive } from "@babel/types";
+import axios from "axios";
+
+// class App extends React.Component {
+// 	state = {
+// 		count: 0
+// 	};
+// 	add = () => {
+// 		this.setState(currnet => ({ count: current.count + 1 }))
+// 	};
+// 	minus = () => {
+// 		this.setState({ count: this.state.count - 1});
+// 	};
+// 	componentDidMount() {
+// 		console.log("Component rendered");
+// 	}
+// 	componentDidUpdate() {
+// 		console.log("I just update");
+// 	}
+// 	componenetWillUnmount() {
+// 		console.log("Goodbye, cruel world");
+// 	}
+// 	render () {
+// 		return (
+// 			<div>
+// 				<h1> The number is: {this.state.count}</h1>
+// 				<button onClick={this.add}>Add</button>
+// 				<button onClick={this.minus}>Minus</button>
+// 			</div>
+// 		);	
+// 	}
+// }
+
 
 class App extends React.Component {
 	state = {
-		count: 0
+		isLoading: true,
+		movies: []
 	};
-	add = () => {
-		this.setState({ count: 1})
-	};
-	minus = () => {
-		this.setState({ count: -1});
-	};
-	render () {
-		return (
-			<div>
-				<h1> The number is: {this.state.count}</h1>
-				<button onClick={this.add}>Add</button>
-				<button onClick={this.minus}>Minus</button>
-			</div>
-		);	
+	getMovies = async () => {
+		const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+	}
+	componentDidMount() {
+		this.getMovies();
+	}
+	render() {
+		const { isLoading } = this.state;
+		return <div>{this.state.isLoading ? "Loading" : "We are ready"}</div>
 	}
 }
-
 export default App;
  
